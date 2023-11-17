12cb4fa2c2138d88acc6dce1c6d36ae8291aa9f6
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-11-17 20:38:12 +0900

    Initial Commit

---
+++ ThreadApp/ThreadAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  ThreadAppApp.swift
+//  ThreadApp
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct ThreadAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
